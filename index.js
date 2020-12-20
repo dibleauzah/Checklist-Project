4fa45560bb72e4e1b7155c7b02b85377aecc08e9
ethanaggor <ethan.aggor@gmail.com>
2020-12-19 22:21:23 -0500

    Document code

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+// Ordered list selector
 const orderedList = document.querySelector("ol");
 
+// This event listener listens for a click event on the "Add task" button
 document.getElementById("button-add").addEventListener("click", function (e) {
   e.preventDefault();
+
+  // Creates a list item & stores the user input bar selector in a variable
   const li = document.createElement("li");
   const inputBar = document.querySelector(".input-item");
 
+  // If the user did not input anything, return
   if (inputBar.value === "") {
     return;
   }
 
+  // Item gets user input and is placed next on the list
   let inputText = inputBar.value;
   li.innerText = inputText;
 
@@ -17,10 +23,13 @@ document.getElementById("button-add").addEventListener("click", function (e) {
   inputBar.value = "";
 });
 
+
+// This event listens for the user to click on an item in the list
 document.getElementById('item-list').addEventListener('click', e => {
   if (e.target && e.target.matches('li')) {
     let li = e.target;
 
+    // Strikesthrough based on if it has a strikethrough or not
     if (li.style.textDecoration == "line-through") {
       li.style.textDecoration = "none";
       li.style.color = "rgb(0, 0, 0)";
@@ -31,9 +40,7 @@ document.getElementById('item-list').addEventListener('click', e => {
   }
 })
 
-// Clear button
-
-
+// Clear button removes all items from the list
 document.querySelector('#button-clear').addEventListener('click', e => {
   while (orderedList.lastElementChild) {
     orderedList.removeChild(orderedList.lastElementChild);
