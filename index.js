9e1eabf7174e03da51f69af5bb985a011cbe7ba8
Seif Sekalala <dibleauzah@gmail.com>
2020-12-19 20:11:35 -0600

    Add item functionality

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-console.log("Testing...");
+const orderedList = document.querySelector("ol");
 
+document.getElementById("button-add").addEventListener("click", function (e) {
+  e.preventDefault();
+  const li = document.createElement("li");
+  //   li.addEventListener("click", strikethrough);
+  const inputBar = document.querySelector(".input-item");
+
+  if (inputBar.value === "") {
+    return;
+  }
+
+  let inputText = inputBar.value;
+  li.innerText = inputText;
+
+  orderedList.appendChild(li);
+
+  inputBar.value = "";
+});
+
+function strikethrough(e) {
+  e.preventDefault();
+
+  if (li.style.textDecoration == "line-through") {
+    li.style.textDecoration = "none";
+  } else {
+    li.style.textDecoration = "line-through";
+  }
+}
